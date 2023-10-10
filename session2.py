b75597d68521e9b2b0717507e1bccd59fdabb7b1
Oleg <omrynskyi@gmail.com>
2023-10-10 11:54:36 -0700

    Session2 for Coding Journal 1
    Date: Tue, Oct 10 11:54

---
+++ session2.py
@@ -0,0 +1,15 @@
+float1 = 45.5
+float2 = 3.98
+
+int1=30
+int2 = 18
+
+float_float = float1 + float2
+
+int_int = int1-int2
+
+float_int = float2*int2
+
+print("Sum of Float",float1,"and Float",float2,":",float_float, type(float_float))
+print("Difference of int",int1,"and int",int2,":",int_int, type(int_int))
+print("Product of Float",float2,"and Int",int2,":",float_int, type(float_int))
