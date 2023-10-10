4b378e7965efd88381401feec98229380258f7a3
Oleg <omrynskyi@gmail.com>
2023-10-10 11:42:57 -0700

    Session 1 for Coding Journal 1
    Date: Tue, Oct 10 11:42pm

---
+++ Session1.py
@@ -0,0 +1,6 @@
+name = "Oleg Mrynskyi"
+pronouns = "he/him"
+fav_movie = "Harry Potter"
+fav_food =  "Pizza"
+
+print("My name is",name+".", "\nMy pronouns are",pronouns+".","\nMy favorite food is",fav_food,"because its delicious.", "\nMy favorite movie is",fav_movie,"and I like to watch it with", fav_food+".")
